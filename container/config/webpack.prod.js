a9427cabb0b40e76500fb72c01e4d24e8a065171
Alice Zhang <ayzhang@ca.ibm.com>
2021-11-25 09:51:50 -0500

    test new

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -14,10 +14,47 @@ const prodConfig = {
   plugins: [
     new ModuleFederationPlugin({
       name: 'container',
-      remotes: {
-        marketing: 'marketing@http://mfe-marketing-node-a-mfe-marketing.apps.ayzhang-1.cp.fyre.ibm.com/remoteEntry.js'
-       // auth2: 'auth2@http://localhost:8082/remoteEntry.js',
-        //dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
+       remotes: {
+        //marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+        marketing: `promise new Promise(resolve => {
+
+    // TODO: in node  0 need to store this in global, so that we only need to load it once
+    var mapUrl = "/public/map.config.json";
+    fetch(mapUrl).then(function(response) {
+      response.json().then(function(data) {
+
+        var  marketingUrl = data.marketing.localhost.href;
+        const script = document.createElement('script')
+        script.src = marketingUrl
+        script.onload = () => {
+            // the injected script has loaded and is available on window
+            // we can now resolve this Promise
+            const proxy = {
+                get: (request) => window.marketing.get(request),
+                init: (arg) => {
+                    try {
+                        return window.marketing.init(arg)
+                    } catch(e) {
+                        console.log('remote container already initialized')
+                    }
+                }
+            }
+            resolve(proxy)
+        }
+        // inject this script with the src set to the versioned remoteEntry.js
+        document.head.appendChild(script);
+      });
+    }
+  )
+         
+
+
+
+
+
+
+      })
+      `,
       },
       shared: packageJson.dependencies,
     }),
